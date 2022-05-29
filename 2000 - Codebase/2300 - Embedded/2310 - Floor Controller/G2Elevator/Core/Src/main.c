e7fc8f1cc3fb3ec1bd29c2c231f5ed62cc7fe5b4
Garnet Koebel <garnetkoebel@koebelstudios.com>
2022-05-29 14:19:05 -0400

    Demo CAN Transmitter
    Basic code for transmitting a floor call request is functional as of this commit

--- 2000 - Codebase/2300 - Embedded/2310 - Floor Controller/G2Elevator/Core/Src/main.c
+++ 2000 - Codebase/2300 - Embedded/2310 - Floor Controller/G2Elevator/Core/Src/main.c
@@ -31,6 +31,13 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define ID					0x0100
+#define GO_TO_FLOOR_1		0x05
+#define GO_TO_FLOOR_2		0x06
+#define GO_TO_FLOOR_3		0x07
+#define NO_BUTTON_PRESSED 	0
+#define BLUE_BUTTON_PRESSED 1
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -46,6 +53,14 @@ I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+CAN_TxHeaderTypeDef TxHeader;
+CAN_RxHeaderTypeDef	RxHeader;
+uint8_t				TxData[8];
+uint8_t				RxData[8];
+uint32_t			TxMailbox;
+
+uint8_t msg = GO_TO_FLOOR_1;
+uint8_t BUTTON = NO_BUTTON_PRESSED;
 
 /* USER CODE END PV */
 
@@ -106,6 +121,30 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  uint8_t i;
+	  //Receive
+	  if (RxData[0] == GO_TO_FLOOR_1) {
+		  //HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+		  HAL_Delay(2000);
+		  for (i=0; i<8; i++) {
+			  RxData[i] = 0x00;
+		  }
+		  //HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+		  HAL_Delay(100);
+	  }
+	  //Transmit
+	  if (BUTTON !=0) {
+		  if (BUTTON == BLUE_BUTTON_PRESSED) {
+			  //HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+			  HAL_Delay(2000);
+			  TxData[0] = msg;
+			  if (HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox) != HAL_OK) {
+				  Error_Handler();
+			  }
+			  //HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+			  BUTTON = NO_BUTTON_PRESSED;
+		  }
+	  }
   }
   /* USER CODE END 3 */
 }
@@ -189,6 +228,37 @@ static void MX_CAN_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN CAN_Init 2 */
+  CAN_FilterTypeDef filter;
+
+  filter.FilterBank = 0;
+  filter.FilterIdHigh = 0x0100 << 5;
+  filter.FilterIdLow = 0x0000;
+  filter.FilterMaskIdHigh = 0xFFC << 5;
+  filter.FilterMaskIdLow = 0x0000;
+  filter.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+  filter.FilterMode = CAN_FILTERMODE_IDMASK;
+  filter.FilterScale = CAN_FILTERSCALE_32BIT;
+  filter.FilterActivation = ENABLE;
+  filter.SlaveStartFilterBank = 0;
+
+  if (HAL_CAN_ConfigFilter(&hcan, &filter) != HAL_OK) {
+	  Error_Handler();
+  }
+
+  if (HAL_CAN_Start(&hcan) != HAL_OK) {
+	  Error_Handler();
+  }
+
+  if (HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK) {
+	  Error_Handler();
+  }
+
+  TxHeader.IDE = CAN_ID_STD;
+  TxHeader.ExtId = 0x00;
+  TxHeader.StdId = ID;
+  TxHeader.RTR = CAN_RTR_DATA;
+  TxHeader.DLC = 1;
+  TxHeader.TransmitGlobalTime = DISABLE;
 
   /* USER CODE END CAN_Init 2 */
 
@@ -325,6 +395,17 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
+	if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) != HAL_OK) {
+		Error_Handler(); // Reception error
+	}
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+	if (GPIO_Pin == GPIO_PIN_13) {
+		BUTTON = BLUE_BUTTON_PRESSED;
+	}
+}
 
 /* USER CODE END 4 */
 
