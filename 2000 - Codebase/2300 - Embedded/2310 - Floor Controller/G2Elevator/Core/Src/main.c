2074d22c85cd37a1083951ec973b6517071fdd91
Giordan Pellegrino <giordanpelle@gmail.com>
2022-05-29 14:08:59 -0400

    added the remaining floor LED pins

--- 2000 - Codebase/2300 - Embedded/2310 - Floor Controller/G2Elevator/Core/Src/main.c
+++ 2000 - Codebase/2300 - Embedded/2310 - Floor Controller/G2Elevator/Core/Src/main.c
@@ -296,7 +296,8 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOC, Debounce2_Pin|Switch_LED_Pin|Floor_Switch_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LD2_Pin|Debouce1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, Floor1LED_Pin|Floor2LED_Pin|Floor3LED_Pin|LD2_Pin
+                          |Debouce1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -311,8 +312,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LD2_Pin Debouce1_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin|Debouce1_Pin;
+  /*Configure GPIO pins : Floor1LED_Pin Floor2LED_Pin Floor3LED_Pin LD2_Pin
+                           Debouce1_Pin */
+  GPIO_InitStruct.Pin = Floor1LED_Pin|Floor2LED_Pin|Floor3LED_Pin|LD2_Pin
+                          |Debouce1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
